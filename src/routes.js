a07bc3a5a2dde025d494d95959c7aa2beb10d490
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-07-13 09:03:00 -0300

    add message hello

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ const MenuController = require('./controllers/MenuController');
 
 const routes = express.Router();
 
+routes.get('/', (req, res) => {
+  return res.json({ message: 'ok' })
+})
+
 routes.post('/menu', MenuController.store);
 
 module.exports = routes;
