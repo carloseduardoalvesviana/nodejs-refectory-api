2f85cb9433d887fe934c6c4305a8befa07144f0b
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-07-13 09:22:02 -0300

    add database connection

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
-
 const app = express();
+
+mongoose.connect('mongodb+srv://ifpi:<password>@clusterifpi.gzbaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useFindAndModify: false, useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true,
+});
+
 app.use(express.json());
 app.use(routes);
-// mongodb+srv://ifpi:<password>@clusterifpi.gzbaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 app.listen(process.env.PORT || 3000);
