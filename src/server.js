0079665e76320c060b615b5e5b05602beb7d9771
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-07-13 09:36:16 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
@@ -6,7 +7,7 @@ const app = express();
 mongoose.connect('mongodb+srv://ifpi:<password>@clusterifpi.gzbaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useFindAndModify: false, useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true,
 });
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
