84cc5b630c1bccfd2c5d335554f5229694da7bd5
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-07-13 09:46:53 -0300

    add infor in qrcode

--- src/controllers/QrcodeController.js
+++ src/controllers/QrcodeController.js
@@ -1,11 +1,15 @@
-const QrCodeModel = require('../models/QrCode');
 var QRCode = require('qrcode')
+const { parseISO } = require('date-fns');
+import { zonedTimeToUtc } from 'date-fns-tz';
 
 class QrCodeController {
   async index(req, res) {
-    // await QrCodeModel.create(req.body);
+    let date = new Date();
+    let dateParser = parseISO(date);
+    const newDate = zonedTimeToUtc(dateParser, 'America/Sao_Paulo');
+
     QRCode.toString('I am a pony!', { type: 'svg' }, function (err, url) {
-      return res.send(url);
+      return res.send(url, newDate);
     });
   }
 }
