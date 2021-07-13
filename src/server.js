9e20d9f0d8636928b280652fd31af43531ee3e41
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-07-13 09:13:16 -0300

    add routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
-
+app.use(express.json());
+app.use(routes);
 // mongodb+srv://ifpi:<password>@clusterifpi.gzbaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
