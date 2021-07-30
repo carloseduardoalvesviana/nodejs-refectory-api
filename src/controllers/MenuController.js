b99698202de21b9c8229d39f297fd1f8ae112b9a
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-07-29 21:24:28 -0300

    alter sequence routes

--- src/controllers/MenuController.js
+++ src/controllers/MenuController.js
@@ -6,17 +6,6 @@ class MenuController {
     return res.json(response);
   }
 
-  async delete(req, res) {
-    try {
-      const {id} = req.body;
-      console.log(id);
-      const response = await MenuModel.findByIdAndDelete({ _id: id });
-      return res.json(response);
-    } catch (error) {
-      return res.status(400).json(error);
-    }
-  }
-
   async store(req, res) {
     const response = await MenuModel.create(req.body);
     return res.json(response);
@@ -51,6 +40,17 @@ class MenuController {
       
     }
    
+  }
+
+  async delete(req, res) {
+    try {
+      const {id} = req.body;
+      console.log(id);
+      const response = await MenuModel.findByIdAndDelete({ _id: id });
+      return res.json(response);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
   }
 }
 
