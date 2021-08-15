9f4c647814fcc98211a19baa42df7ffc8c6054f5
= <=>
2021-08-15 11:18:08 -0300

    fix qrcode

--- src/controllers/QrcodeController.js
+++ src/controllers/QrcodeController.js
@@ -1,17 +1,10 @@
 class QrCodeController {
+
   async index(req, res) {
-    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-    var randomPassword;
-
-    for (var i = 0; i < 16; i++) {
-      randomPassword += chars.charAt(
-        Math.floor(Math.random() * chars.length)
-      );
-
-      return res.status(200).json(randomPassword);
-    }
-
+    var randomPassword = Math.random().toString(36).slice(-8);
+    return res.status(200).json(randomPassword);
   }
+
 }
 
 module.exports = new QrCodeController();
