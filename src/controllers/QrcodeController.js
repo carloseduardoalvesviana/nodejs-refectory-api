8f5f5ec87d64098f6bbb0fabbb2fb5fde691ad7d
= <=>
2021-08-15 11:11:45 -0300

    fix error qrcode

--- src/controllers/QrcodeController.js
+++ src/controllers/QrcodeController.js
@@ -1,15 +1,15 @@
 class QrCodeController {
   async index(req, res) {
     let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-    var randomPassword;
-
+  
     for (var i = 0; i < 16; i++) {
-      randomPassword += chars.charAt(
+      var randomPassword = chars.charAt(
         Math.floor(Math.random() * chars.length)
       );
+      
+      return res.status(200).json(randomPassword);
     }
 
-    return res.status(200).json(randomPassword);
   }
 }
 
