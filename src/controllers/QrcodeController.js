8b42a6cc0b167064a8a9027f90ed053336d2f966
= <=>
2021-08-15 11:08:19 -0300

    add random qrcode

--- src/controllers/QrcodeController.js
+++ src/controllers/QrcodeController.js
@@ -1,10 +1,10 @@
 class QrCodeController {
   async index(req, res) {
     let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-    let randomPassword;
+    var randomPassword;
 
     for (var i = 0; i < 16; i++) {
-      randomPassword = randomPassword + chars.charAt(
+      randomPassword += chars.charAt(
         Math.floor(Math.random() * chars.length)
       );
     }
