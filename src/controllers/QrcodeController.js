51c9a1062408a9f09cd4aa74df24ca879eb037da
= <=>
2021-08-15 11:13:26 -0300

    fix error random char

--- src/controllers/QrcodeController.js
+++ src/controllers/QrcodeController.js
@@ -1,12 +1,13 @@
 class QrCodeController {
   async index(req, res) {
     let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-  
+    var randomPassword;
+
     for (var i = 0; i < 16; i++) {
-      var randomPassword = chars.charAt(
+      randomPassword += chars.charAt(
         Math.floor(Math.random() * chars.length)
       );
-      
+
       return res.status(200).json(randomPassword);
     }
 
