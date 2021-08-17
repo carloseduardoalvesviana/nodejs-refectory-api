ab0e2daaa55d6637325c9952d14fa9bd4da38a7d
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-08-17 10:14:22 -0300

    add find one student

--- src/routes.js
+++ src/routes.js
@@ -17,6 +17,7 @@ const MessageController = require('./controllers/MessageController');
 routes.get('/', (req, res) => res.json({ message: 'Welcome to IFPI API' }));
 routes.post('/csv/import', upload.single('csv'), CsvController.read);
 
+// Menu
 routes.post('/menu', MenuController.store);
 routes.get('/menu', MenuController.findMenuOfDay);
 routes.get('/menu/all', MenuController.index);
@@ -24,22 +25,28 @@ routes.put('/menu', MenuController.update);
 routes.delete('/menu', MenuController.delete);
 routes.get('/menu/:id', MenuController.findOne);
 
+// Qrcode
 routes.get('/qrcode', QrcodeController.index);
 routes.post('/qrcode/reserve/:id', QrcodeController.confirm);
 
-routes.get('/students', StudentController.index);
+// Permissions
 routes.put('/students/permission/yes/:id', PermissionController.update);
 routes.put('/students/permission/no/:id', PermissionController.updateNo);
+
+// Students
+routes.get('/students', StudentController.index);
 routes.post('/students', StudentController.store);
-routes.delete('/students/:id', StudentController.delete);
 routes.put('/students/:id', StudentController.update);
 routes.get('/students/:id', StudentController.findOne);
+routes.delete('/students/:id', StudentController.delete);
 
+// Reserves
 routes.post('/menu/reserve/:id', ReserveController.store);
 routes.put('/reserves/cancel/:id', ReserveController.cancel);
-routes.get('/reserves', ReserveController.index);
-routes.post('/login', SessionController.store);
 routes.post('/reserves/find/:id', ReserveController.find);
+routes.get('/reserves', ReserveController.index);
+
+routes.post('/login', SessionController.store);
 
 routes.post('/message', MessageController.store);
 
