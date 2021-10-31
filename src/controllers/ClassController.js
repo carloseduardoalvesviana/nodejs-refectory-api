58b1545040a58d24778875b90af86bd3d1e449d3
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-10-31 14:32:28 -0300

    add delete method in class controller

--- src/controllers/ClassController.js
+++ src/controllers/ClassController.js
@@ -3,23 +3,24 @@ const CourseSchema = require('../models/Course');
 
 class ClassController {
   async index(req, res) {
-    const classes = await ClassSchema.find()
-      .populate('course')
-      .exec();
+    const classes = await ClassSchema.find().populate('course').exec();
     return res.status(200).json(classes);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const r = await ClassSchema.findOneAndDelete({ _id: id });
+    return res.json(r);
   }
 
   async store(req, res) {
     const { shift, course, year } = req.body;
-
     const course_ = await CourseSchema.findOne({ _id: course });
-
     const class_ = await ClassSchema.create({
       shift: shift,
       course: course_._id,
       year: year,
     });
-
     return res.json(class_);
   }
 }
