67bc90532525fd2912871ab95ae9f6c7fce0406a
carlos.viana <30371938+carloseduardoalvesviana@users.noreply.github.com>
2021-11-13 15:27:08 -0300

    Update server.js
    comment connection mongodb

--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 
+/*
 mongoose.connect('mongodb+srv://ifpi:ifpi@clusterifpi.gzbaa.mongodb.net/refeitorio?retryWrites=true&w=majority', {
   useFindAndModify: false,
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+*/
 
 app.use(cors());
 app.use(express.json());
