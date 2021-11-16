f4821f16cfbd793ac5967d1e724f8972f4bd65b1
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-11-16 18:33:27 -0300

    deploy to ifpi

--- src/routes.js
+++ src/routes.js
@@ -17,103 +17,102 @@ const TeacherController = require('./controllers/TeacherController');
 const CourseController = require('./controllers/CourseController');
 const ClassController = require('./controllers/ClassController');
 const AdminController = require('./controllers/AdminController');
-// const ClassManagementController = require('./controllers/ClassManagementController');
 const ClassReservation = require('./controllers/ClassReservation');
 
 // Entry point API
-routes.get('/', (req, res) => res.json({ message: 'Welcome to api' }));
+routes.get('/node', (req, res) => res.json({ message: 'Welcome to api' }));
 
 // Class - turmas
-routes.post('/class', ClassController.store);
-routes.get('/class', ClassController.index);
-routes.get('/students/class/:id_class', ClassController.getStudents);
-routes.delete('/class/:id', ClassController.delete);
-routes.get('/class/:id', ClassController.findOne);
-routes.put('/class/:id', ClassController.update);
+routes.post('/node/class', ClassController.store);
+routes.get('/node/class', ClassController.index);
+routes.get('/node/students/class/:id_class', ClassController.getStudents);
+routes.delete('/node/class/:id', ClassController.delete);
+routes.get('/node/class/:id', ClassController.findOne);
+routes.put('/node/class/:id', ClassController.update);
 
 // Class - agendamento
-routes.put('/reservations/disapprove/:id', ClassReservation.disapprove);
-routes.post('/class/reservation', ClassReservation.reservation);
-routes.get('/reservations', ClassReservation.index);
-routes.put('/reservations/:id', ClassReservation.update)
-routes.get('/reservations/:id', ClassReservation.getReservastionByTeacherId);
-routes.get('/reservationDetail/:id', ClassReservation.reservationDetail);
-routes.delete('/reservations/:id', ClassReservation.delete);
-routes.put('/reservations/class/students/:id', ClassReservation.addStudents);
+routes.put('/node/reservations/disapprove/:id', ClassReservation.disapprove);
+routes.post('/node/class/reservation', ClassReservation.reservation);
+routes.get('/node/reservations', ClassReservation.index);
+routes.put('/node/reservations/:id', ClassReservation.update)
+routes.get('/node/reservations/:id', ClassReservation.getReservastionByTeacherId);
+routes.delete('/node/reservations/:id', ClassReservation.delete);
+
+routes.get('/node/reservationDetail/:id', ClassReservation.reservationDetail);
+routes.put('/node/reservations/class/students/:id', ClassReservation.addStudents);
 
 // Class admin
-routes.post('/admin/class/reservation', ClassReservation.adminReservationStore);
-routes.get('/admin/reservations/:id', ClassReservation.getReservastionByAdminId);
-routes.put('/admin/reservations/class/:id', ClassReservation.addStudentsAdmin);
-routes.delete('/admin/reservations/class/:id', ClassReservation.deleteAdmin);
+routes.post('/node/admin/class/reservation', ClassReservation.adminReservationStore);
+routes.get('/node/admin/reservations/:id', ClassReservation.getReservastionByAdminId);
+routes.put('/node/admin/reservations/class/:id', ClassReservation.addStudentsAdmin);
+routes.delete('/node/admin/reservations/class/:id', ClassReservation.deleteAdmin);
 
 // Course
-routes.post('/courses', CourseController.store);
-routes.get('/courses', CourseController.get);
-routes.delete('/courses/:id', CourseController.delete);
-routes.put('/courses/:id', CourseController.update);
-routes.get('/courses/:id', CourseController.findOne);
-
+routes.post('/node/courses', CourseController.store);
+routes.get('/node/courses', CourseController.get);
+routes.delete('/node/courses/:id', CourseController.delete);
+routes.put('/node/courses/:id', CourseController.update);
+routes.get('/node/courses/:id', CourseController.findOne);
 
 // Login
-routes.post('/login/student', SessionController.auth);
-routes.post('/auth/professor', TeacherController.auth);
-routes.post('/auth/admin', AdminController.auth);
-routes.get('/students/found', StudentController.findNotConfirm);
+routes.post('/node/login/student', SessionController.auth);
+routes.post('/node/auth/professor', TeacherController.auth);
+routes.post('/node/auth/admin', AdminController.auth);
+routes.get('/node/students/found', StudentController.findNotConfirm);
 
 // Message
-routes.post('/message', MessageController.store);
+routes.post('/node/message', MessageController.store);
 
 // Menu
-routes.post('/menu', MenuController.store);
-routes.get('/menu', MenuController.findMenuOfDay);
-routes.get('/menu/all', MenuController.index);
-routes.put('/menu', MenuController.update);
-routes.delete('/menu', MenuController.delete);
-routes.get('/menu/:id', MenuController.findOne);
+routes.post('/node/menu', MenuController.store);
+routes.get('/node/menu', MenuController.findMenuOfDay);
+routes.get('/node/menu/all', MenuController.index);
+routes.put('/node/menu', MenuController.update);
+routes.delete('/node/menu', MenuController.delete);
+routes.get('/node/menu/:id', MenuController.findOne);
 
 // Qrcode
-routes.get('/qrcode', QrcodeController.index);
-routes.post('/qrcode/reserve/:id', QrcodeController.confirm);
+routes.get('/node/qrcode', QrcodeController.index);
+routes.post('/node/qrcode/reserve/:id', QrcodeController.confirm);
 
 // Permissions
-routes.put('/students/permission/yes/:id', PermissionController.update);
-routes.put('/students/permission/no/:id', PermissionController.updateNo);
+routes.put('/node/students/permission/yes/:id', PermissionController.update);
+routes.put('/node/students/permission/no/:id', PermissionController.updateNo);
 
 // Students
-routes.get('/students', StudentController.index);
-routes.put('/students/:id', StudentController.update);
-routes.get('/students/:id', StudentController.findOne);
-routes.delete('/students/:id', StudentController.delete);
-routes.get('/students/class/:id', StudentController.findStudentByClass);
-routes.post('/students', StudentController.store);
+routes.get('/node/students', StudentController.index);
+routes.put('/node/students/:id', StudentController.update);
+routes.get('/node/students/:id', StudentController.findOne);
+routes.delete('/node/students/:id', StudentController.delete);
+routes.get('/node/students/class/:id', StudentController.findStudentByClass);
+routes.post('/node/students', StudentController.store);
 
-routes.put('/reservations/student/:id', ReserveController.confirm);
-routes.put('/reservations/student/disapprove/:id', ReserveController.disapprove);
-routes.delete('/reservations/student/:id', ReserveController.delete);
+routes.put('/node/reservations/student/:id', ReserveController.confirm);
+routes.put('/node/reservations/student/disapprove/:id', ReserveController.disapprove);
+routes.delete('/node/reservations/student/:id', ReserveController.delete);
 
 // Teachers
-routes.post('/teachers', TeacherController.store);
-routes.get('/teachers', TeacherController.index);
-routes.put('/teachers/:id', TeacherController.update);
-routes.delete('/teachers/:id', TeacherController.delete);
-routes.get('/teachers/:id', TeacherController.findOne);
+routes.post('/node/teachers', TeacherController.store);
+routes.get('/node/teachers', TeacherController.index);
+routes.put('/node/teachers/:id', TeacherController.update);
+routes.delete('/node/teachers/:id', TeacherController.delete);
+routes.get('/node/teachers/:id', TeacherController.findOne);
 
 // Admins
-routes.post('/admin/store', AdminController.store);
-routes.get('/admins/index', AdminController.index);
-routes.put('/admin/update/:id', AdminController.update);
-routes.delete('/admin/delete/:id', AdminController.delete);
-routes.get('/admin/find/:id', AdminController.findOne);
+routes.post('/node/admin/store', AdminController.store);
+routes.get('/node/admins/index', AdminController.index);
+routes.put('/node/admin/update/:id', AdminController.update);
+routes.delete('/node/admin/delete/:id', AdminController.delete);
+routes.get('/node/admin/find/:id', AdminController.findOne);
 
 // Import Student
-routes.post('/csv/import', upload.single('csv'), CsvController.read);
+routes.post('/node/csv/import', upload.single('csv'), CsvController.read);
 
 // Reserves
-routes.post('/menu/reserve/:id', ReserveController.store);
-routes.put('/reserves/cancel/:id', ReserveController.cancel);
-routes.post('/reserves/find/:id', ReserveController.find);
-routes.get('/reserves', ReserveController.index);
+routes.post('/node/menu/reserve/:id', ReserveController.store);
+routes.put('/node/reserves/cancel/:id', ReserveController.cancel);
+routes.post('/node/reserves/find/:id', ReserveController.find);
+routes.get('/node/reserves', ReserveController.index);
 
 // listen
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -4,18 +4,20 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 
-mongoose.connect('mongodb+srv://ifpi:ifpi@clusterifpi.gzbaa.mongodb.net/refeitorio?retryWrites=true&w=majority', {
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect('mongodb+srv://ifpi:ifpi@clusterifpi.gzbaa.mongodb.net/refeitorio?retryWrites=true&w=majority', {
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect('mongodb://127.0.0.1:27017/ifpi_refeitorio');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen(process.env.PORT || 3334, () => {
   console.log('api running');
 });
