cb83383a2e5dc2039339e91b1a1b44bfcb3264db
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-12-04 17:17:01 -0300

    population in notification student login

--- src/controllers/NotificationStudentLoginController.js
+++ src/controllers/NotificationStudentLoginController.js
@@ -3,7 +3,7 @@ const NotificationStudentLogin = require('../models/NotificationStudentLogin');
 class NotificationStudentLoginControllerController {
   async index(req, res) {
     try {
-      const notifications = await NotificationStudentLogin.find();
+      const notifications = await NotificationStudentLogin.find().populate('id_student').exec();
       return res.status(200).json(notifications);
     } catch (error) {
       return res.status(400).json(error);
