db623e9e4ff07010cffceb571fdd4b7f322606e1
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-12-04 17:23:30 -0300

    add try catch to auth student

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -2,9 +2,14 @@ const Student = require('../models/Student');
 
 class SessionController {
   async auth(req, res) {
-    const { code } = req.body;
-    const response = await Student.findOne({ code: code });
-    return res.status(200).json(response);
+    try {
+      const { code } = req.body;
+      const response = await Student.findOne({ code: code });
+      return res.status(200).json(response);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+
   }
 
 }
