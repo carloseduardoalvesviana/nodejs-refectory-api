93cceb1acc6ddee5369cf7e8fc051ef2ce05b125
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-12-04 17:25:08 -0300

    add mongoose conection

--- src/server.js
+++ src/server.js
@@ -4,14 +4,14 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 
-// mongoose.connect('mongodb+srv://ifpi:ifpi@clusterifpi.gzbaa.mongodb.net/refeitorio?retryWrites=true&w=majority', {
-//   useFindAndModify: false,
-//   useCreateIndex: true,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect('mongodb+srv://ifpi:ifpi@clusterifpi.gzbaa.mongodb.net/refeitorio?retryWrites=true&w=majority', {
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect('mongodb://127.0.0.1:27017/ifpi_refeitorio');
+// mongoose.connect('mongodb://127.0.0.1:27017/ifpi_refeitorio');
 
 app.use(cors());
 app.use(express.json());
